ec1c1ca2c1251fa473eb89178b97bd026db238d1
Boris Dudkin <boris.dudkin@gmail.com>
2023-03-16 10:21:47 -0400

    initial commit - app.py created and structure added, images and data set added

--- README.md
+++ README.md
@@ -1,1 +1,239 @@
-# FinTech_Project_2_Airbnb_Price_Prediction
+# Investment Advisor
+
+### The Investment Advisor was developed to help empower potential investors by providing portfolio suggestions from just a few questions about income and attitude towards investment.The Investment Advisor application will assess an investor's risk tolerance and his/her's capacity to absorb risk. Based on those evaluations, two corresponding risk scores will be calculated and associated investment portfolios will be chosen from our ETFs offering. A third 'Cryptomix' portfolio including cryptocurrency assets will also be included. Their performance will be assessed and compared to the Benchmark portfolio of 40% Bonds 60% Stocks.
+
+---
+
+![ETF](Images/Inv_advice.png)
+
+---
+
+## Table of contents
+
+1. [Technologies](#technologies)
+2. [Installation Guide](#installation-guide)
+3. [Usage](#usage)
+4. [Contributors](#contributors)
+5. [License](#license)
+
+---
+
+## Technologies
+
+`Python 3.9`
+
+`Jupyter lab`
+
+_Libraries_
+
+1. `Pandas` is a Python package that provides fast, flexible, and expressive data structures designed to make working with large sets of data easy and intuitive.
+
+   - [pandas](https://github.com/pandas-dev/pandas) - for the documentation, installation guide and dependencies.
+
+2. `PyViz` is a Python visualization package that provides a single platform for accessing multiple visualization libraries. The visualization libraries used in this application are listed below: <br/>
+
+   - [PyViz ](https://pyviz.org/) - for guidance on how to start visualization, interactive visualization, styles and layouts customization.
+   - [Bokeh](https://bokeh.org/) - is an open source vizualization library that allows developers to create interactive plots. 
+   - [Holoviews](https://holoviews.org/) - is an open source vizualization library that allows developers to create interactive plots, designed to interact with tabular data. 
+   - [Plotly](https://plotly.com/) - is a library that allows developers to build interactive charts, tables and graphs from data.
+
+3. `SQLAlchemy` is an open-source SQL library for Python. It is designed to ease the communication between Python-based programs and databases"
+
+   - [SQLAlchemy ](https://www.sqlalchemy.org/) - for information on the library, its features and installation instructions.<br/>
+
+4. `Streamlit` is a library that allows developers to build web applications with live user input.
+
+   - [Streamlit](https://streamlit.io/) - to read more about deploying, installing and customizing.<br/>
+   
+5. `NumPy` is an open source library that adds computational tools to a project, including mathematical functions and random number generators.
+
+   - [Numpy](https://numpy.org/) - to read about available functions and installation.<br/>
+   
+6. `Python-dotenv` is a library that adds functions to read locally stored .env files.
+
+   - [python-dotenv](https://pypi.org/project/python-dotenv/) - to read about installation and usage.<br/>
+   
+7. `Alpaca` is a company that creates API-centered tools to trade stocks and buy cryptocurrency.
+
+   - [Alpaca](https://alpaca.markets/) - to sign up and read about installation and usage.<br/>
+   
+ 
+---
+
+## Installation Guide
+
+To run this program the application must be started from the terminal using Streamlit.<br/> A .env file must also be written and stored in the local directory before running the program according to the instructions below.<br/> Once the installations below are completed and the .env file has been made, navigate to the folder containing the application and run the following command in your terminal to launch the program:
+
+```python
+streamlit run investment_app.py
+```
+
+Jupyter lab is the preferred software to work with this application.<br/> Jupyter lab is a part of the **[anaconda](https://www.anaconda.com/)** distribution package and therefore it is recommended to download **anaconda** first.<br/> Once downloaded, run the following command in your terminal to launch Jupyter lab:
+
+```python
+jupyter lab
+```
+### Creating the .env file
+To create the .env file, open a .txt document and type in your keys from the Alpaca dashboard after creating an account in the following format:
+
+![env](Images/env.PNG)
+
+Rename the text file to .env and save the file to the 'Boris' Directory after selecting 'All Files':
+
+![env_save](Images/env_save.PNG)
+
+### Library Installations
+Before using the application first install the following dependencies by using your terminal:
+
+To install pandas run:
+
+```python
+pip install pandas
+```
+
+```python
+# or conda
+conda install pandas
+```
+
+To install the PyViz visualizations, in Terminal run:
+
+```python
+# conda
+conda install -c pyviz bokeh holoviews plotly
+```
+
+Confirm the installation of all the PyViz packages by running the following commands in Terminal:
+```python
+conda list pyviz
+```
+```python
+ conda list bokeh
+```
+```python
+ conda list holoviews
+```
+```python
+ conda list plotly
+```
+
+To install SQLAlchemy, in Terminal run:
+
+```python
+pip install SQLAlchemy
+```
+
+Confirm the installation of the SQLAlchemy package by running the following commands in Terminal:
+
+```python
+ conda list sqlalchemy
+```
+
+To install Streamlit, in Terminal run:
+
+```python
+pip install streamlit
+```
+
+Confirm the installation of the Streamlit package by running the following commands in Terminal:
+
+```python
+ conda list streamlit
+```
+
+To install python-dotenv, in Terminal run:
+
+```python
+pip install python-dotenv
+```
+
+Confirm the installation of the python-dotenv package by running the following commands in Terminal:
+
+```python
+ conda list python-dotenv
+```
+
+To install numpy, in Terminal run:
+
+```python
+pip install numpy
+```
+
+Confirm the installation of the numpy package by running the following commands in Terminal:
+
+```python
+ conda list numpy
+```
+
+To install the Alpaca Trade API, in Terminal run:
+
+```python
+pip3 install alpaca-trade-api
+```
+
+Confirm the installation of the Alpaca Trade API package by running the following command in Terminal:
+
+```python
+ conda list alpaca-trade-api
+```
+---
+
+## Usage
+
+> Application summary<br/>
+
+The Investment Advisor tool helps investors make educated decisions based on their risk capacity and risk tolerance, just by asking a few questions.<br/>
+Furthermore, Streamlit library transforms Investment Advisor into an interactive web application that nontechnical users without any coding experience can use.<br/> Answer a few questions about your income and attitude towards investment, and the application will provide three portfolio recommendations as well as a benchmark portfolio
+
+- The analysis begins by asking the user some questions:<br/>
+  ![questionnaire](Images/questionnaire.PNG)<br/>
+- Next, risk capacity and risk tolerance scores are calculated:<br/>
+  ![risk_scores](Images/risk_scores.PNG)<br/>
+- The calculated portfolios can be compared side by side at a glance:<br/>
+  ![portfolios_comparison](Images/portfolios_comparison.PNG)<br/>
+- You can find more details about each portfolio by using a dropdown menu:<br/>
+  ![portfolio_composition](Images/portfolio_composition.PNG)<br/>
+- The historical performance of each asset is shown to the user in an easy to read chart:<br/>
+  ![historical_performance](Images/historical_performance.PNG)<br/>
+- A Monte Carlo simulation can be run on each portfolio selected, and the estimated returns will be displayed:<br/>
+  ![simulation_1](Images/simulation_1.PNG)<br/>
+  ![simulation_2](Images/simulation_2.PNG)<br/>
+
+
+
+https://user-images.githubusercontent.com/118786747/215371133-e1c822cd-d054-4f2e-80d0-c34880016df5.mp4
+
+
+
+> Getting started<br/>
+
+- To use the Investment Advisor first clone the repository to your PC.<br/>
+- Use `streamlit run investment_app.py` as per the instructions in the [Installation Guide](#installation-guide) to run the application.<br/>
+
+---
+
+## Contributors
+
+Contact Details:
+
+Boris Dudkin:
+
+- [Email](boris.dudkin@gmail.com)
+- [LinkedIn](www.linkedin.com/in/Boris-Dudkin)
+
+Brian Wander:
+
+- [Email](brianwander101@gmail.com)
+
+Adam Jimenez:
+
+- [Email](adamjimenez642@gmail.com)
+- [LinkedIn](linkedin.com/in/adam-jimenez)
+
+---
+
+## License
+
+MIT
+
+---
---
+++ app.py
@@ -0,0 +1,327 @@
+from pathlib import Path
+
+import chart_studio.plotly as py
+import cufflinks as cf
+import hvplot.pandas
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+import streamlit as st
+import tensorflow as tf
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+# init_notebook_mode(connected=True)
+# cf.go_offline()
+
+#create internet page name
+st.set_page_config(page_title="AirBnB Price Prediction with ML", layout='wide', initial_sidebar_state="collapsed")
+
+@st.cache_data
+def get_data():
+    df = pd.read_csv(
+    Path("Resources/air_bnb.csv"))
+    return df
+
+
+# Create four tabs to display our application as per below breakdown
+tab1, tab2, tab3, tab4 = st.tabs(['Introduction','Data Analysis and Cleansing','Exploratory Data Analysis','Machine Learning'])
+
+with tab1:
+    df_original = get_data()
+    bnb_df=df_original.copy()
+    icon,title=st.columns([1,20])
+    with icon: 
+        st.image('Images/bnb.png', use_column_width=True)
+    with title:
+        st.title('Introduction')
+
+    st.header('About This Application:')
+
+    my_text = "to be completed.."
+
+    st.write(my_text)
+
+    # col1,col2=st.columns([1,2])    
+    # with col1:
+    st.subheader('Dataset:')
+
+    my_text_data = "to be completed."
+
+    st.write(my_text_data)
+    # with col2:
+
+        # st.subheader('Explore Listings by Location - Original data:')
+        # city_selection2 = st.selectbox("Select a city:", tuple(bnb_df['city'].unique()))
+        # fig_map = px.density_mapbox(bnb_df.loc[bnb_df['city']==city_selection2], lat = 'lat', lon = 'lng', z = 'realSum', radius = 10, center = dict(lat=bnb_df.loc[bnb_df['city']==city_selection2]['lat'].mean(), lon = bnb_df.loc[bnb_df['city']==city_selection2]['lng'].mean()),
+        #                     zoom = 10, hover_name='room_type', mapbox_style = 'open-street-map',
+        #                 title = city_selection2+ ' Price per listing',labels={'realSum': 'Listing Price' })
+        # st.plotly_chart(fig_map,use_container_width=True)
+
+    st.subheader('Application Setup:')
+
+    my_text_application = "to be completed."
+
+    st.write(my_text_application)
+
+    st.subheader('Other Areas to Investigate:')
+
+    my_text_other = "to be completed."
+
+    st.write(my_text_other)
+
+
+
+with tab2:
+
+    icon,title=st.columns([1,20])
+    with icon: 
+        st.image('Images/home.png', use_column_width=True)
+    with title:
+        st.title('Initial Data Analysis')
+
+
+    st.header('Original Data Review and Cleansing:')
+    # df_original = get_data()
+
+    st.subheader('Original Dataset:')
+    st.dataframe(df_original,use_container_width=True)
+
+    #make a copy of the original dataframe
+    # bnb_df=df_original.copy()
+
+    # remove redundant columns
+    bnb_df=bnb_df.drop(columns=['room_shared','room_private','attr_index','attr_index_norm','rest_index','rest_index_norm', 'lng', 'lat'])
+
+    # bnb_df=bnb_df.drop(columns=['room_shared','room_private','attr_index','attr_index_norm','rest_index','rest_index_norm'])    
+
+    # rename columns with more descriptive names:
+    bnb_df.rename(columns = {'day':'day_of_week','realSum':'listing_price', 'multi':'multiple_rooms ',
+                              'biz':' business_facilities', 'bedrooms':'bedrooms_quantity', 'dist':'city_center_distance', 'metro_dist':'metro_distance'}, inplace = True)
+
+    #modify columns with Boolean data:
+    bnb_df['host_is_superhost']=bnb_df['host_is_superhost'].apply(lambda x:1 if x==True else 0)
+
+    st.subheader('Modified Dataset')
+    st.caption('NOTE: Redundant columns removed, descriptive names given to columns, boolean type columns modified.')
+    st.dataframe(bnb_df,use_container_width=True)
+
+    st.subheader('Descriptive Statistics:')
+    st.write(bnb_df.describe(),use_container_width=True)
+
+
+    st.subheader('Dataset Deepdive:')
+    col1,col2,col3,col4 = st.columns(4, gap='large')
+    
+    with col1:
+        st.markdown('#### Dataset Shape:')
+        st.write(bnb_df.shape,use_container_width=True)
+        st.caption('The modified dataset has 51,707 rows and 13 columns.')
+    with col2:
+        st.markdown(' #### Duplicated Rows - Totals:')
+        st.write(bnb_df.duplicated().sum(),use_container_width=True)
+        st.caption('There are no duplicated rows.')
+    with col3:
+        st.markdown(' #### Misssing Values - Totals:')
+        st.write(bnb_df.isnull().sum(),use_container_width=True)
+        st.caption('There are no missing values.')
+    with col4:
+        st.markdown(' #### Columns Datatypes:')
+        st.write(bnb_df.dtypes,use_container_width=True)
+        st.caption('There are three columns with cathegorical data: day_of_week, room_type and city, which is in line with expectations.')
+    
+    st.subheader('Data Distirbution and Outliers:')
+
+     # select column to analize value_counts()
+    column_selection = st.selectbox("Select a column to analyze the number of unique values and distribution:", tuple(bnb_df.columns))
+    
+    col1,col2 = st.columns(2, gap='large')
+  
+    with col1:
+        # st.write(bnb_df[column_selection].value_counts())
+        fig = px.bar(bnb_df[column_selection].value_counts(), x=column_selection, labels={'index': 'Unique Values' }, title = column_selection + ' - Unique Values Count')
+        fig.update_layout(uniformtext_minsize=8)
+        st.plotly_chart(fig,use_container_width=True)
+
+    with col2:
+        fig_hist=px.histogram(bnb_df, x=column_selection, nbins=11, title=column_selection + ' Histogram', marginal = 'violin')
+        st.plotly_chart(fig_hist,use_container_width=True)
+
+    st.subheader('Listing Prices Outliers:')
+    city_selection = st.selectbox("Select a city to analyse further the listing price outliers:", tuple(bnb_df['city'].unique()))
+
+    col1,col2 = st.columns(2, gap='large')
+    with col1:
+        # price_outliers=px.violin(bnb_df, x='city', y='listing_price', box=True, 
+        #         color='day_of_week', points='all', hover_data=bnb_df.columns,
+        #         labels={'city': 'City','listing_price': 'Listing Price' }, title = 'Listing Price Distribution per city')
+        # st.plotly_chart(price_outliers,use_container_width=True)
+        # st.markdown(f' ##### Total Listing Price Quantiles')
+        # st.write(bnb_df['listing_price'].quantile([0.01, 0.25, 0.5, 0.75, 0.99]))
+        fig_quantile=px.bar(bnb_df.loc[bnb_df['city']==city_selection]['listing_price'].quantile([0.01, 0.25, 0.5, 0.75, 0.99]),labels={'index': 'Quantile', 'value': 'Listing Price' }, title = city_selection + ' - Listing Price Quantile Distribution')
+        fig.update_layout(showlegend=False)
+        st.plotly_chart(fig_quantile,use_container_width=True)
+
+        st.caption('Based on the distribution analysis the lisiting prices above **:blue[700]** can be considered as outliers and will be removed for the Exploratory Data Analysis.')
+
+    with col2:
+        # city_selection = st.selectbox("Select a city to analyse further the listing price outliers:", tuple(bnb_df['city'].unique()))
+
+        fig_city=go.Figure()
+        fig_city.add_trace(go.Violin(x=bnb_df['city'][bnb_df['city']==city_selection],
+                       y=bnb_df['listing_price'][bnb_df['day_of_week']=='weekday'],
+                       legendgroup='Yes', scalegroup='Yes', name='weekday',
+                       side='negative', line_color='blue'))
+        fig_city.add_trace(go.Violin(x=bnb_df['city'][bnb_df['city']==city_selection],
+                       y=bnb_df['listing_price'][bnb_df['day_of_week']=='weekend'],
+                       legendgroup='Yes', scalegroup='Yes', name='weekend',
+                       side='positive', line_color='red'))
+        fig_city.update_layout(title=city_selection +' City Violin Plot')
+        st.plotly_chart(fig_city,use_container_width=True)
+        st.markdown(f' ##### {city_selection} Listing Price Quantiles')
+        # st.write(bnb_df.loc[bnb_df['city']==city_selection]['listing_price'].quantile([0.01, 0.25, 0.5, 0.75, 0.99]))
+    
+    # remove outliers (listing prices above 750):
+    df=bnb_df.loc[bnb_df['listing_price']<=700]
+    
+    price_outliers_new=px.violin(df, x='city', y='listing_price', box=True, 
+                color='day_of_week', points='all', hover_data=df.columns,
+                labels={'city': 'City','listing_price': 'Listing Price' }, title = 'Listing Price Distribution per city (corrected for outliers)')
+    st.plotly_chart(price_outliers_new,use_container_width=True)
+   
+    
+with tab3:
+    # Create a list of categorical variables 
+    categorical_variables = list(df.dtypes[df.dtypes == "object"].index)
+    # Create a dataframewith categorical variables 
+    df_categrical=df[categorical_variables]
+    
+    # Create a dDataframe with non-categorical variables:
+    df_numerical=df.drop(columns = categorical_variables)
+
+    icon,title=st.columns([1,20])
+    with icon: 
+        st.image('Images/rent.png', use_column_width=True)
+    with title:
+        st.title('Exploratory Data Analysis')
+
+
+    st.subheader('Listing Price and Categorical Features Analysis (per City by Day of Week or Room Type):')
+
+    col1,col2 = st.columns(2, gap='large')
+    with col1:
+        df_city_day=df.groupby(by=['city','day_of_week'])['listing_price'].mean().reset_index().sort_values(by=['listing_price'])
+        df_city_day['listing_price']=round(df_city_day['listing_price'],0)
+        city_day_fig=px.bar(df_city_day, x='city', y='listing_price', color='day_of_week', barmode='group', title = 'Average Listing Prices per City by Day of Week', text='listing_price', labels={'city': 'City', 'day_of_week': 'Day of the week', 'listing_price': 'Price' })
+        city_day_fig.update_traces(texttemplate='%{text:2s}', textposition='outside')
+        st.plotly_chart(city_day_fig,use_container_width=True)
+
+    with col2:
+        
+        df_city_room=df.groupby(by=['city','room_type'])['listing_price'].mean().reset_index().sort_values(by=['listing_price'])
+        df_city_room['listing_price']=round(df_city_room['listing_price'],0)
+        city_room_fig=px.bar(df_city_room, x='city', y='listing_price', color='room_type', barmode='group', title = 'Average Listing Prices per City by Room Type', text='listing_price', labels={'city': 'City', 'room_type': 'Room Type', 'listing_price': 'Price' })
+        city_room_fig.update_traces(texttemplate='%{text:2s}', textposition='outside')
+        st.plotly_chart(city_room_fig,use_container_width=True)
+
+    st.subheader('Listing Price (Target) / Features Regression Analysis:')
+    city_regression = st.selectbox("Select a city:", tuple(df_categrical
+    ['city'].unique()))
+    df_city=df.loc[df['city']==city_regression]
+
+    col1,col2=st.columns([1,4])
+    columns_list=['day_of_week','host_is_superhost','multiple_rooms ',' business_facilities']
+    xscale_list=['person_capacity','cleanliness_rating','guest_satisfaction_overall','bedrooms_quantity', 'city_center_distance', 'metro_distance']
+    # df_regression=df.copy()
+
+    with col1:
+        x_scale = st.selectbox("Select a feature for x scale:", tuple(xscale_list))
+        z_scale = st.selectbox("Select a feature for columns:", tuple(columns_list))
+        # color = st.selectbox("Select a feature for color:", tuple(df['city'].columns))
+    with col2:
+        
+        fig_regression=px.scatter(df_city, x=x_scale, y='listing_price', color = df_city['room_type'], hover_data=['guest_satisfaction_overall','listing_price','bedrooms_quantity','cleanliness_rating'],
+           labels={'room_type' : 'Room Type','listing_price': 'Listing Price', 'day_of_week': 'Day of Week', 'bedrooms_quantity':'Number of Bedrooms'},
+              color_continuous_scale=px.colors.sequential.Viridis,
+              facet_col=z_scale, title='Target/Feature Regression Analysis')
+
+        st.plotly_chart(fig_regression,use_container_width=True)    
+
+       # fig_regression_features=px.scatter(df_city, x=x_scale, y=y_scale,  hover_data=['guest_satisfaction_overall','listing_price','bedrooms_quantity','cleanliness_rating'],
+        #    labels={'room_type' : 'Room Type','listing_price': 'Listing Price', 'day_of_week': 'Day of Week', 'bedrooms_quantity':'Number of Bedrooms'}, title='Features Regression Analysis')
+
+    
+    st.subheader(city_regression+  ' Listing Price (Target) / Features Correlation Analysis:')
+    col1,col2 = st.columns(2, gap='large')
+    with col1:
+        corr_df = df_city.corr()[['listing_price']].sort_values(by='listing_price', ascending=False)
+        # fig_corr=plt.figure()
+        # sns.heatmap(corr_df.round(2), annot=True, vmin=-1, vmax=1, center =0, cmap='vlag')
+        # sns.set_theme(style='dark')
+        # st.pyplot(fig_corr,use_container_width=True)  
+        st.markdown(' ##### Target/Features Correlations:')
+        fig = px.imshow(corr_df.round(2), color_continuous_scale='Viridis', aspect="auto", text_auto=".2f")
+        fig.update_xaxes(side="top")
+        st.plotly_chart(fig,use_container_width=True)
+        st.caption('**NOTE: The correlation between the Target and the Features as well as between the Features is :blue[quite low]. Therefore, Neuaral Network and Decision Tree Machine Learning Models will be selected.**')
+
+    with col2:
+        st.markdown(' ##### Features Correlations:')
+
+        corr_m = df_city.corr()
+        # fig_corr=plt.figure()
+        # sns.heatmap(corr_m.round(2), annot=True, vmin=-1, vmax=1, center =0, cmap='vlag')
+        # sns.set_theme(style='dark')
+        # st.pyplot(fig_corr,use_container_width=True)  
+
+        # fig = px.imshow(corr_m.round(2), color_continuous_scale='Viridis', aspect="auto", text_auto=".2f")
+        # fig.update_xaxes(side="top")
+
+        # Correlation
+        df_corr =corr_m.round(2)
+        # Mask to matrix
+        mask = np.zeros_like(df_corr, dtype=bool)
+        mask[np.triu_indices_from(mask)] = True
+        # Viz
+        df_corr_viz = df_corr.mask(mask).dropna(how='all').dropna('columns', how='all')
+        fig = px.imshow(df_corr_viz, text_auto=True, color_continuous_scale='Viridis')
+        # fig.update_xaxes(side="top")
+        st.plotly_chart(fig,use_container_width=True) 
+    # with col1:
+    #     x_scale = st.selectbox("Select a feature for x scale:", tuple(bnb_df.columns))
+    #     y_scale = st.selectbox("Select a feature for y scale:", tuple(bnb_df.columns))
+    #     z_scale = st.selectbox("Select a feature for z scale:", tuple(bnb_df.columns))
+    #     color = st.selectbox("Select a feature for color:", tuple(bnb_df.columns))
+    # with col2:
+    #     fig_3d = px.scatter_3d(df, x=x_scale, y=y_scale,  z=z_scale, color = color, opacity=0.7, hover_data=['guest_satisfaction_overall','listing_price','bedrooms_quantity','cleanliness_rating'],
+    #         labels={'room_type' : 'Room Type', 'listing_price': 'Listing Price' })
+    #     st.plotly_chart(fig_3d,use_container_width=True)
+    # city_selection2 = st.selectbox("Select a city:", tuple(df['city'].unique()))
+    # st.markdown(' ####  Day of Week:')
+    # weekday_selection = st.selectbox("Select a day of week :", tuple(df['day_of_week'].unique()))
+    # st.markdown(' #### Select Room Type:')
+    # room_selection = st.selectbox("Select a room type :", tuple(df['room_type'].unique()))
+
+    # st.header('Original Data Review and Cleansing:')
+
+
+
+
+
+    
+with tab4:
+
+    icon,title=st.columns([1,20])
+    with icon: 
+        st.image('Images/house.png', use_column_width=True)
+    with title:
+        st.title('Machine Learning and Price Prediction')
+
+
+    st.header('Original Data Review and Cleansing:')
